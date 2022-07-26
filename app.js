56c19f2b0459785f45ba19c3fc82a80fae25cb67
YouMe6110 <ume0116@naver.com>
2022-07-27 00:04:40 +0900

    DAY 04

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const COLORS = [
     { r: 255, g: 104, b: 248 },
     { r: 44, g: 209, b: 252 },
     { r: 54, g: 233, b: 84 },
-]
+];
 
 class App {
     constructor() {
@@ -14,12 +14,12 @@ class App {
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
 
-        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+        this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
 
-        this.totalParticles = 1;
+        this.totalParticles = 15;
         this.particles = [];
-        this.maxRadius = 90;
-        this.minRadius = 40;
+        this.maxRadius = 900;
+        this.minRadius = 400;
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
@@ -34,6 +34,8 @@ class App {
         this.canvas.width = this.stageWidth * this.pixelRatio;
         this.canvas.height = this.stageHeight * this.pixelRatio;
         this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        this.ctx.globalCompositeOperation = 'saturation';
 
         this.createParticles();
     }
